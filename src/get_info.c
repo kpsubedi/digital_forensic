2400479a375f46136d20a8aeb67a2b4f9c7716ca
Kul Subedi <subedi.kulprasad@gmail.com>
2015-12-18 10:22:32 -0600

    file type

--- src/get_info.c
+++ src/get_info.c
@@ -22,16 +22,49 @@ int main(int argc, char *argv[]){
     printf("File stat mode_t :%x\n", (mode_t)filestat.st_mode);
 
     printf("File type:		\n");
-    switch (filestat.st_mode & S_IFMT){
+    /*switch (filestat.st_mode & S_IFMT){
 	case S_IFDIR:
 		printf("Directory \n");
+		break;
+	case S_IFREG:
+		printf("Regular File\n");
+		break;
+	case S_IFBLK:
+		printf("Block Device\n");
+		break;
+	case S_IFCHR:
+		printf("Character Device\n");
+		break;
+	case S_IFFIFO:
+		printf("FIFO/Pipe\n");
+		break;
+	case S_IFLNK:
+		printf("Symbolic Link \n");
+		break;
+	case S_IFSOCK:
+		printf("Socket\n");
 		break;
 	default:
 		printf("Unknown\n");
 		break;
 
-	}
-
+	}*/
+    if(S_ISREG(filestat.st_mode))
+        printf("Regular File\n");
+    else if(S_ISDIR(filestat.st_mode))
+        printf("Directory \n");
+    else if(S_ISCHR(filestat.st_mode))
+	printf("Character Device\n");
+    else if(S_ISBLK(filestat.st_mode))
+	printf("Block Device\n");
+    else if(S_ISFIFO(filestat.st_mode))
+	printf("FIFO/PIPE \n");
+    else if(S_ISSOCK(filestat.st_mode))
+	printf("Socket\n");
+    else if(S_ISLNK(filestat.st_mode))
+        printf("Symbolic Link\n");		
+    else
+	printf("Unknown\n");
     return 0;
 }
 	
